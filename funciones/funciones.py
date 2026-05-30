726281c8108d2493d2cf50c623ae6f5d69c600d7
cona-pixel <cons.castroo@duocuc.cl>
2026-05-30 11:22:49 -0400

    ejermplos de funciones

---
+++ funciones/funciones.py
@@ -0,0 +1,23 @@
+#funciones
+
+def sumarDosnumeros():
+    """Esta funcion permite sumas dos numeros,
+    ingresandos dentro de la funcion
+    """
+    num1=int(input("Ingrese numero 1: "))
+    num2=int(input("Ingrese numero 2: "))
+    suma=num1+num2
+    print(f"la suma de {num1}+{num2} es= {suma}")
+
+    def sumar (a,b):
+        return suma
+    
+# linea principal
+sumarDosnumeros()
+
+res= sumar (4,5)
+print(res)
+
+
+
+
